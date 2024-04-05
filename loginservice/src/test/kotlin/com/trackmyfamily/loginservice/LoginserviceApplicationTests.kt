1866ae897fd2131897a45241496c886f50bbe94e
unknown <swapnilgupta.iiith@gmail.com>
2024-04-05 23:47:10 +0530

    Adding login service initial code

---
+++ loginservice/src/test/kotlin/com/trackmyfamily/loginservice/LoginserviceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.trackmyfamily.loginservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LoginserviceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
