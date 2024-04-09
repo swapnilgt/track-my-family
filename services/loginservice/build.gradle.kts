a031ec0d791aae592c954b118a14aba81387590e
unknown <swapnilgupta.iiith@gmail.com>
2024-04-09 13:58:46 +0530

    Standalone gradle build success for both modules

--- services/loginservice/build.gradle.kts
+++ services/loginservice/build.gradle.kts
@@ -2,7 +2,6 @@ plugins {
     java
     id("org.springframework.boot") version "3.2.4"
     id("io.spring.dependency-management") version "1.1.4"
-    id("java")
     kotlin("jvm") version "1.9.23"
 }
 
@@ -10,7 +9,12 @@ group = "com.trackmyfamily"
 version = "0.0.1-SNAPSHOT"
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_17
+}
+
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
 }
 
 repositories {
@@ -18,17 +22,18 @@ repositories {
 }
 
 dependencies {
-    implementation(platform("org.springframework.boot:spring-boot-dependencies:3.2.4"))
-    implementation("org.springframework.boot:spring-boot")
+    implementation("org.springframework.boot:spring-boot-starter")
+    //implementation(":commons")
+    compileOnly("org.projectlombok:lombok")
+    runtimeOnly("org.postgresql:postgresql")
+    annotationProcessor("org.projectlombok:lombok")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("junit:junit:4.13.2")
     implementation(kotlin("stdlib-jdk8"))
 }
 
 tasks.withType<Test> {
     useJUnitPlatform()
 }
-
-tasks.withType<JavaCompile> {
-    options.compilerArgs.add("-parameters")
+kotlin {
+    jvmToolchain(17)
 }
---
+++ services/loginservice/src/test/java/com/trackmyfamily/loginservice/ExampleTest.kt
@@ -0,0 +1,13 @@
+package com.trackmyfamily.loginservice
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+
+class ExampleTest {
+
+    @Test
+    fun exmampleTest() {
+        Assertions.assertEquals(4, 2 + 2)
+    }
+}
